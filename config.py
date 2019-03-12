e08c69dd919436076efe1dbc23e138057e6843c4
Cactiw <knyazev1514@gmail.com>
2019-03-12 21:22:31 +0300

    Немного почистил код

--- config.py
+++
@@ -1,5 +0,0 @@
-psql_creditals = {'dbname' : 'db_name', 'user' : 'username', 'pass' : 'password'}
-
-Production_token = 'Token here'
-
-#request_kwargs = {'proxy_url': 'socks5h://163.172.152.192:1080'}    #Configured proxy - if you need it
