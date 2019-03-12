aca5b631532f220ff0150caa6fed537cd7625cd6
M9SCO <48180788+M9SCO@users.noreply.github.com>
2019-03-12 22:14:07 +0600

    Update bot.py
    #925 cтр

--- bot.py
+++ bot.py
@@ -922,6 +922,20 @@ def textMessage(bot, update):
                                 guild_reports.add_report(current_report)
                                 reports_count.update({guild_tag : guild_reports})
                                 chat_id = guilds_chat_ids.get(guild_tag)
+                                #by M_9sco
+                                if guild_reports.num_reports == 1:
+                                    response = '{0} \n \n 🏅 это самый первый репорт после битвы '.format( response)
+                                    SELECT = "SELECT date_in FROM reportsWHERE user_id={0} AND battle_id= {1}'".format(mes.from_user.id, battle_id)
+                                    cursor.execute(SELECT)
+                                    row = cursor.fetchone()
+                                    first_report = "🏅 " + str(row[0])
+                                    register = "UPDATE reports SET(date_in={2}) WHERE user_id={0} AND battle_id= {1}  ;".format(mes.from_user.id, battle_id, first_report)
+                                    cursor.execute(register)
+                                    row1 = cursor.fetchone()
+                                    cnx.commit()
+                                
+                                
+                                
                                 if chat_id is not None:
                                     percent = (guild_reports.num_reports / guild_reports.num_players) * 100
                                     response = "Репорт от <b>{0}</b> принят.\nВсего сдало репортов <b>{1}</b> человек, это <b>{2:.2f}</b>% " \
