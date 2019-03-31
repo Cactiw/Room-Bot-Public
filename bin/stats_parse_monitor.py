8645122fc9348ba85aa9f90855651fa78a99ed33
cactiw <knyazev1514@gmail.com>
2019-03-31 18:35:21 +0300

    Бот теперь тихо пинит топ по битве

--- bin/stats_parse_monitor.py
+++ bin/stats_parse_monitor.py
@@ -1,5 +1,6 @@
 from work_materials.globals import castles_stats_queue, SQUAD_GUILDS_TAGS, STATS_SEND_CHAT_ID, dispatcher, admin_ids
 import re
+from telegram.error import TelegramError
 
 
 def parse_stats():
@@ -21,5 +22,10 @@ def parse_stats():
                         response += "{}{} <b>{}</b>\n".format("🛡️" if nickname[0] == attacked_castle else"⚔️",
                                                               attacked_castle, nickname[:-1])
         if response != "":
-            dispatcher.bot.send_message(chat_id=STATS_SEND_CHAT_ID, text=response, parse_mode='HTML')
+            message = dispatcher.bot.sync_send_message(chat_id=STATS_SEND_CHAT_ID, text=response, parse_mode='HTML')
+            try:
+                dispatcher.bot.pin_chat_message(chat_id=STATS_SEND_CHAT_ID, message_id=message.message_id,
+                                                disable_notification=True)
+            except TelegramError:
+                pass
         data = castles_stats_queue.get()
